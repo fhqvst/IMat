2bf2a9e605045f4e42c239cbfac298ab82d95508
Henrik Nilson <henrik.nilson91@gmail.com>
2015-02-26 15:08:52 +0100

    added some filters

--- iMat/src/imat/FilterFactory.java
+++ iMat/src/imat/FilterFactory.java
@@ -18,9 +18,15 @@ public class FilterFactory {
     protected static FilterPanel dairyFilterCard;
     protected static FilterPanel veggieAndFruitsFilterCard;
     protected static FilterPanel sweetsFilterCard;
+    protected static FilterPanel freezerFilterCard;
+    protected static FilterPanel pantryFilterCard;
     
     protected static void createFilterCards() {
-        meatAndFishFilterCard = new FilterPanel("Kött&Fisk", new String[]{"Fläskkött","Nötkött", "Fisk"});
+        meatAndFishFilterCard = new FilterPanel("Kött & Fisk", new String[]{"Fläskkött","Nötkött", "Fisk"});
         dairyFilterCard = new FilterPanel("Kylvaror", new String[]{"Mjölk"});
+        veggieAndFruitsFilterCard = new FilterPanel("Frukt & Grönt", new String[]{"Grönsaker", "Frukt"});
+        sweetsFilterCard = new FilterPanel("Godis och Snacks", new String[]{"Godis"});
+        freezerFilterCard = new FilterPanel("Frysvaror", new String[]{"Frysta saker"});
+        pantryFilterCard = new FilterPanel("Skafferi", new String[]{"Mjöl","Socker","Kakao"});
     }
 }
