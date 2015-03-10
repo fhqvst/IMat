5f5dd0170d8c9e21beb42bad3af01aa19e0183f9
Kakan-IT14 <jesper.olsson191@gmail.com>
2015-03-10 16:29:20 +0100

    changed shopping lists representation

--- iMat/src/imat/MyPreviousShopping.java
+++ iMat/src/imat/MyPreviousShopping.java
@@ -25,6 +25,8 @@ public class MyPreviousShopping extends javax.swing.JPanel implements java.beans
      */
     public MyPreviousShopping() {
         initComponents();
+        myStoredListsLabel.setText("<html><h2>Mina inköpslistor</h2></html>");
+        myLatestShoppingsLabel.setText("<html><h2>Mina senaste köp</h2></html>");
         orders = new DefaultListModel();
         items = new DefaultListModel();
         for(int i = 0; i < dataHandler.getOrders().size(); i ++){
@@ -54,24 +56,21 @@ public class MyPreviousShopping extends javax.swing.JPanel implements java.beans
 
         myStoredListsScrollPane = new javax.swing.JScrollPane();
         myStoredListsPanel = new javax.swing.JPanel();
-        jLabel55 = new javax.swing.JLabel();
-        jButton21 = new javax.swing.JButton();
-        jButton25 = new javax.swing.JButton();
-        jButton27 = new javax.swing.JButton();
-        jButton28 = new javax.swing.JButton();
-        jToggleButton4 = new javax.swing.JToggleButton();
-        jToggleButton5 = new javax.swing.JToggleButton();
-        jLabel54 = new javax.swing.JLabel();
-        jLabel56 = new javax.swing.JLabel();
-        jComboBox10 = new javax.swing.JComboBox();
-        jLabel57 = new javax.swing.JLabel();
+        myStoredListsLabel = new javax.swing.JLabel();
+        myLatestShoppingsLabel = new javax.swing.JLabel();
         jScrollPane3 = new javax.swing.JScrollPane();
         ordersList = new javax.swing.JList();
-        cartButtonList1 = new javax.swing.JButton();
-        cartButtonList2 = new javax.swing.JButton();
         jScrollPane4 = new javax.swing.JScrollPane();
         itemsList = new javax.swing.JList();
         filler4 = new javax.swing.Box.Filler(new java.awt.Dimension(20, 0), new java.awt.Dimension(20, 0), new java.awt.Dimension(20, 0));
+        jScrollPane5 = new javax.swing.JScrollPane();
+        ordersList1 = new javax.swing.JList();
+        jScrollPane6 = new javax.swing.JScrollPane();
+        itemsList1 = new javax.swing.JList();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setBorder(null);
         setLayout(new java.awt.GridLayout(1, 0));
@@ -82,36 +81,11 @@ public class MyPreviousShopping extends javax.swing.JPanel implements java.beans
 
         myStoredListsPanel.setPreferredSize(new java.awt.Dimension(250, 250));
 
-        jLabel55.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel55.setText("Mina inköpslistor");
+        myStoredListsLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        myStoredListsLabel.setText("Mina inköpslistor");
 
-        jButton21.setText("+");
-
-        jButton25.setText("Veckohandel");
-
-        jButton27.setText("+");
-
-        jButton28.setText("Middagsbjudning");
-
-        jToggleButton4.setText("[auto]");
-        jToggleButton4.setToolTipText("Lägg till som autoköp");
-
-        jToggleButton5.setText("[auto]");
-        jToggleButton5.setToolTipText("Lägg till som autoköp");
-        jToggleButton5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jToggleButton5ActionPerformed(evt);
-            }
-        });
-
-        jLabel54.setText("[auto]");
-
-        jLabel56.setText("autoköp");
-
-        jComboBox10.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "varje tisdag" }));
-
-        jLabel57.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel57.setText("Mina senaste köp");
+        myLatestShoppingsLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        myLatestShoppingsLabel.setText("Mina senaste köp");
 
         ordersList.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
             public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
@@ -120,25 +94,28 @@ public class MyPreviousShopping extends javax.swing.JPanel implements java.beans
         });
         jScrollPane3.setViewportView(ordersList);
 
-        cartButtonList1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imat/resources/basket.png"))); // NOI18N
-        cartButtonList1.setBorderPainted(false);
-        cartButtonList1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        cartButtonList1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cartButtonList1ActionPerformed(evt);
-            }
-        });
-
-        cartButtonList2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imat/resources/basket.png"))); // NOI18N
-        cartButtonList2.setBorderPainted(false);
-        cartButtonList2.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        cartButtonList2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cartButtonList2ActionPerformed(evt);
-            }
-        });
-
         jScrollPane4.setViewportView(itemsList);
+
+        ordersList1.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                ordersList1ValueChanged(evt);
+            }
+        });
+        jScrollPane5.setViewportView(ordersList1);
+
+        jScrollPane6.setViewportView(itemsList1);
+
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Namn");
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Innehåll");
+
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("När");
+
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Handlade produkter");
 
         javax.swing.GroupLayout myStoredListsPanelLayout = new javax.swing.GroupLayout(myStoredListsPanel);
         myStoredListsPanel.setLayout(myStoredListsPanelLayout);
@@ -147,93 +124,56 @@ public class MyPreviousShopping extends javax.swing.JPanel implements java.beans
             .addGroup(myStoredListsPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addGroup(myStoredListsPanelLayout.createSequentialGroup()
-                            .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(myStoredListsPanelLayout.createSequentialGroup()
-                                    .addComponent(jButton27)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jButton28))
-                                .addGroup(myStoredListsPanelLayout.createSequentialGroup()
-                                    .addComponent(jButton21)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jButton25)))
-                            .addGap(49, 49, 49)
-                            .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(cartButtonList2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                                .addComponent(cartButtonList1, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(myStoredListsPanelLayout.createSequentialGroup()
-                                    .addGap(18, 18, 18)
-                                    .addComponent(jToggleButton5))
-                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, myStoredListsPanelLayout.createSequentialGroup()
-                                    .addGap(18, 18, 18)
-                                    .addComponent(jToggleButton4))))
-                        .addGroup(myStoredListsPanelLayout.createSequentialGroup()
-                            .addComponent(jLabel54)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(jLabel56)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(jComboBox10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 280, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(jLabel57, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 280, Short.MAX_VALUE))
-                        .addComponent(jLabel55, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(filler4, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(0, 38, Short.MAX_VALUE))
+                    .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(myLatestShoppingsLabel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 325, Short.MAX_VALUE)
+                        .addComponent(myStoredListsLabel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jScrollPane5, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jScrollPane6, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jScrollPane4, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING))
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(filler4, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         myStoredListsPanelLayout.setVerticalGroup(
             myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(myStoredListsPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel55)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jButton21)
-                        .addComponent(jButton25))
-                    .addComponent(cartButtonList1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                    .addComponent(jToggleButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(myStoredListsLabel)
                 .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jToggleButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jButton27)
-                        .addComponent(jButton28))
-                    .addComponent(cartButtonList2, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(myStoredListsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel54)
-                    .addComponent(jLabel56)
-                    .addComponent(jComboBox10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(filler4, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(myStoredListsPanelLayout.createSequentialGroup()
+                        .addGap(121, 121, 121)
+                        .addComponent(filler4, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(myStoredListsPanelLayout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(myLatestShoppingsLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel57)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(47, Short.MAX_VALUE))
+                .addContainerGap(157, Short.MAX_VALUE))
         );
 
         myStoredListsScrollPane.setViewportView(myStoredListsPanel);
 
         add(myStoredListsScrollPane);
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jToggleButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton5ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jToggleButton5ActionPerformed
-
-    private void cartButtonList1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cartButtonList1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_cartButtonList1ActionPerformed
-
-    private void cartButtonList2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cartButtonList2ActionPerformed
-
-    }//GEN-LAST:event_cartButtonList2ActionPerformed
 
     private void ordersListValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_ordersListValueChanged
         
@@ -249,27 +189,28 @@ public class MyPreviousShopping extends javax.swing.JPanel implements java.beans
         }
     }//GEN-LAST:event_ordersListValueChanged
 
+    private void ordersList1ValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_ordersList1ValueChanged
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ordersList1ValueChanged
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton cartButtonList1;
-    private javax.swing.JButton cartButtonList2;
     private javax.swing.Box.Filler filler4;
     private javax.swing.JList itemsList;
-    private javax.swing.JButton jButton21;
-    private javax.swing.JButton jButton25;
-    private javax.swing.JButton jButton27;
-    private javax.swing.JButton jButton28;
-    private javax.swing.JComboBox jComboBox10;
-    private javax.swing.JLabel jLabel54;
-    private javax.swing.JLabel jLabel55;
-    private javax.swing.JLabel jLabel56;
-    private javax.swing.JLabel jLabel57;
+    private javax.swing.JList itemsList1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JScrollPane jScrollPane4;
-    private javax.swing.JToggleButton jToggleButton4;
-    private javax.swing.JToggleButton jToggleButton5;
+    private javax.swing.JScrollPane jScrollPane5;
+    private javax.swing.JScrollPane jScrollPane6;
+    private javax.swing.JLabel myLatestShoppingsLabel;
+    private javax.swing.JLabel myStoredListsLabel;
     private javax.swing.JPanel myStoredListsPanel;
     private javax.swing.JScrollPane myStoredListsScrollPane;
     private javax.swing.JList ordersList;
+    private javax.swing.JList ordersList1;
     // End of variables declaration//GEN-END:variables
 }
