50be7eae296d4beb3258327159a99b9a4dc76cf2
Filip Hallqvist <hej@filiphallqvist.se>
2015-03-11 14:15:28 +0100

    Filterpanel changes

--- iMat/src/imat/FilterPanel.java
+++ iMat/src/imat/FilterPanel.java
@@ -128,6 +128,7 @@ public class FilterPanel extends javax.swing.JPanel implements java.beans.Custom
         jLabel1 = new javax.swing.JLabel();
         filterPanel = new javax.swing.JPanel();
         unselectAllButton = new javax.swing.JButton();
+        filler1 = new javax.swing.Box.Filler(new java.awt.Dimension(0, 10), new java.awt.Dimension(0, 10), new java.awt.Dimension(0, 10));
         selectAllButton = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(255, 255, 255));
@@ -137,17 +138,20 @@ public class FilterPanel extends javax.swing.JPanel implements java.beans.Custom
         setPreferredSize(new java.awt.Dimension(300, 434));
         setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.Y_AXIS));
 
-        titleLable.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
+        titleLable.setFont(new java.awt.Font("Helvetica", 1, 18)); // NOI18N
         titleLable.setText("Title");
+        titleLable.setVerticalAlignment(javax.swing.SwingConstants.TOP);
         titleLable.setAlignmentX(0.5F);
         titleLable.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
         titleLable.setMaximumSize(new java.awt.Dimension(1337, 20));
         titleLable.setMinimumSize(new java.awt.Dimension(0, 0));
-        titleLable.setPreferredSize(new java.awt.Dimension(1337, 20));
+        titleLable.setPreferredSize(new java.awt.Dimension(1337, 30));
+        titleLable.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
         add(titleLable);
 
-        jSpinner1.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 0, 0, 0));
+        jSpinner1.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(200, 200, 200)));
         jSpinner1.setMaximumSize(new java.awt.Dimension(32767, 60));
+        jSpinner1.setPreferredSize(new java.awt.Dimension(37, 60));
         jSpinner1.setValue(150);
         add(jSpinner1);
 
@@ -163,27 +167,44 @@ public class FilterPanel extends javax.swing.JPanel implements java.beans.Custom
         filterPanel.setBackground(new java.awt.Color(255, 255, 255));
         add(filterPanel);
 
+        unselectAllButton.setFont(new java.awt.Font("Helvetica", 1, 14)); // NOI18N
+        unselectAllButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imat/resources/button.png"))); // NOI18N
+        unselectAllButton.setText("AVMARKERA ALLT");
         unselectAllButton.setAlignmentX(0.5F);
-        unselectAllButton.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        unselectAllButton.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(200, 200, 200)));
+        unselectAllButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         unselectAllButton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        unselectAllButton.setLabel("Avmarkera allt");
-        unselectAllButton.setMaximumSize(new java.awt.Dimension(1337, 60));
-        unselectAllButton.setMinimumSize(new java.awt.Dimension(0, 0));
+        unselectAllButton.setMargin(null);
+        unselectAllButton.setMaximumSize(new java.awt.Dimension(1337, 40));
+        unselectAllButton.setMinimumSize(new java.awt.Dimension(1337, 40));
         unselectAllButton.setPreferredSize(new java.awt.Dimension(1337, 40));
+        unselectAllButton.setRolloverEnabled(true);
+        unselectAllButton.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/imat/resources/button-hover.png"))); // NOI18N
+        unselectAllButton.setRolloverSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/imat/resources/button-hover.png"))); // NOI18N
+        unselectAllButton.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/imat/resources/button-selected.png"))); // NOI18N
         unselectAllButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 unselectAllButtonActionPerformed(evt);
             }
         });
         add(unselectAllButton);
+        add(filler1);
 
-        selectAllButton.setText("Återställ filtrering");
+        selectAllButton.setFont(new java.awt.Font("Helvetica", 1, 14)); // NOI18N
+        selectAllButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imat/resources/button.png"))); // NOI18N
+        selectAllButton.setText("ÅTERSTÄLL FILTRERING");
         selectAllButton.setAlignmentX(0.5F);
-        selectAllButton.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        selectAllButton.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(200, 200, 200)));
+        selectAllButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         selectAllButton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        selectAllButton.setMaximumSize(new java.awt.Dimension(1337, 60));
-        selectAllButton.setMinimumSize(new java.awt.Dimension(0, 0));
+        selectAllButton.setMargin(null);
+        selectAllButton.setMaximumSize(new java.awt.Dimension(1337, 40));
+        selectAllButton.setMinimumSize(new java.awt.Dimension(1337, 40));
         selectAllButton.setPreferredSize(new java.awt.Dimension(1337, 40));
+        selectAllButton.setRolloverEnabled(true);
+        selectAllButton.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/imat/resources/button-hover.png"))); // NOI18N
+        selectAllButton.setRolloverSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/imat/resources/button-hover.png"))); // NOI18N
+        selectAllButton.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/imat/resources/button-selected.png"))); // NOI18N
         selectAllButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 selectAllButtonActionPerformed(evt);
@@ -206,6 +227,7 @@ public class FilterPanel extends javax.swing.JPanel implements java.beans.Custom
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.Box.Filler filler1;
     private javax.swing.JPanel filterPanel;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JSpinner jSpinner1;
