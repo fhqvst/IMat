95f2b716d46411f07a8ee35dce20e14fcfdd3e79
Alexander Sandberg <alex95sandberg@gmail.com>
2015-03-11 13:43:32 +0100

    Added sorting to productpanel, no implementation

--- iMat/src/imat/ProductCategoryPanel.java
+++ iMat/src/imat/ProductCategoryPanel.java
@@ -115,6 +115,20 @@ public class ProductCategoryPanel extends javax.swing.JPanel implements java.bea
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jPanel1 = new javax.swing.JPanel();
+        sortingPanel = new javax.swing.JPanel();
+        leftPanel = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        nameLinkLabel = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        priceLinkLabel = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        favouritesLinkLabel = new javax.swing.JLabel();
+        rightPanel = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jToggleButton1 = new javax.swing.JToggleButton();
+        jToggleButton2 = new javax.swing.JToggleButton();
         productsScrollPane = new javax.swing.JScrollPane();
         productsPanel = new javax.swing.JPanel();
 
@@ -123,6 +137,65 @@ public class ProductCategoryPanel extends javax.swing.JPanel implements java.bea
         setMinimumSize(new java.awt.Dimension(0, 0));
         setPreferredSize(new java.awt.Dimension(100, 200));
         setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.LINE_AXIS));
+
+        jPanel1.setLayout(new javax.swing.BoxLayout(jPanel1, javax.swing.BoxLayout.Y_AXIS));
+
+        sortingPanel.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 1, 0, 0, new java.awt.Color(200, 200, 200)));
+        sortingPanel.setLayout(new java.awt.GridLayout(1, 2));
+
+        leftPanel.setBackground(new java.awt.Color(255, 255, 255));
+        leftPanel.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+
+        jLabel1.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel1.setText("Sortera efter:");
+        leftPanel.add(jLabel1);
+
+        nameLinkLabel.setForeground(new java.awt.Color(102, 102, 255));
+        nameLinkLabel.setText("Namn");
+        nameLinkLabel.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        leftPanel.add(nameLinkLabel);
+
+        jLabel3.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel3.setText("|");
+        leftPanel.add(jLabel3);
+
+        priceLinkLabel.setForeground(new java.awt.Color(102, 102, 255));
+        priceLinkLabel.setText("Pris");
+        priceLinkLabel.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        leftPanel.add(priceLinkLabel);
+
+        jLabel5.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel5.setText("|");
+        leftPanel.add(jLabel5);
+
+        favouritesLinkLabel.setForeground(new java.awt.Color(102, 102, 255));
+        favouritesLinkLabel.setText("Favoriter");
+        favouritesLinkLabel.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        leftPanel.add(favouritesLinkLabel);
+
+        sortingPanel.add(leftPanel);
+
+        rightPanel.setBackground(new java.awt.Color(255, 255, 255));
+        rightPanel.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.RIGHT));
+
+        jLabel2.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel2.setText("Vy:");
+        rightPanel.add(jLabel2);
+
+        buttonGroup1.add(jToggleButton1);
+        jToggleButton1.setSelected(true);
+        jToggleButton1.setText("Ruta");
+        jToggleButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        rightPanel.add(jToggleButton1);
+
+        buttonGroup1.add(jToggleButton2);
+        jToggleButton2.setText("Lista");
+        jToggleButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        rightPanel.add(jToggleButton2);
+
+        sortingPanel.add(rightPanel);
+
+        jPanel1.add(sortingPanel);
 
         productsScrollPane.setBorder(null);
         productsScrollPane.setMaximumSize(new java.awt.Dimension(2147483647, 2147483647));
@@ -135,12 +208,28 @@ public class ProductCategoryPanel extends javax.swing.JPanel implements java.bea
         productsPanel.setLayout(new java.awt.GridLayout(0, 4, 20, 20));
         productsScrollPane.setViewportView(productsPanel);
 
-        add(productsScrollPane);
+        jPanel1.add(productsScrollPane);
+
+        add(jPanel1);
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JLabel favouritesLinkLabel;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JToggleButton jToggleButton1;
+    private javax.swing.JToggleButton jToggleButton2;
+    private javax.swing.JPanel leftPanel;
+    private javax.swing.JLabel nameLinkLabel;
+    private javax.swing.JLabel priceLinkLabel;
     private javax.swing.JPanel productsPanel;
     private javax.swing.JScrollPane productsScrollPane;
+    private javax.swing.JPanel rightPanel;
+    private javax.swing.JPanel sortingPanel;
     // End of variables declaration//GEN-END:variables
 }
